21d2083f3a8bbb8aa21844fc7e6eb575c45faf9c
neodivert <neodivert@gmail.com>
2013-05-24 19:33:53 +0100

    - Añadadido código de matrices y pruebas (olvidado en un commit anterior).

---
+++ drawingRobot/src/matrix.hpp
@@ -0,0 +1,36 @@
+#ifndef MATRIX_HPP
+#define MATRIX_CPP
+/*
+    Transformation matrix.
+*/
+
+#include <ofMain.h>
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+class Matrix {
+	private:
+		float m[3][3];
+    public:
+        // 1. Initialization
+        Matrix(); // Same as SetIdentity();
+        void setIdentity();
+        void setZeros();
+
+        // 2. Transformations
+		void setTranslation( float tx, float ty );
+		void setRotation( float angle );
+		void setScale( float sx, float sy );
+
+        // 3, Operators
+        Matrix& operator * (const Matrix& b);
+		float& operator()(int i, int j){ return m[i][j]; }
+		float operator()(int i, int j) const { return m[i][j]; }
+};
+
+// 4. << operator
+ostream& operator<< (ostream &out, const Matrix &matrix);
+
+#endif
+// MATRIX_HPP
---
+++ drawingRobot/src/tests.hpp
@@ -0,0 +1,26 @@
+#ifndef TESTS_H
+#define TESTS_H
+
+#include "polygon.hpp"
+#include <cassert>
+
+class Test
+{
+    private:
+        Vertex originalVertex, resultVertex;
+        Matrix transMatrix;
+        float x, y, tx, ty, sx, sy, angle;
+
+    public:
+        void testAll();
+        void testTranslations();
+        void testRotations();
+        void testScales();
+
+        void testCompoundTrasnformations();
+};
+
+
+
+
+#endif // TESTS_H
